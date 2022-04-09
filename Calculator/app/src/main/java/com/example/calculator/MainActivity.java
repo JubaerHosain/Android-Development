458a49301f81a19cb925293cb423e645429e9b12
jubaer <jubaerhosain1119@gmail.com>
2022-04-09 23:19:22 +0600

    Changed launcher icon for Calculator

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,75 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editTextFirstValue, editTextSecondValue;
+    TextView textViewAnswer;
+    Button add, subtract, multiply, divide;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextFirstValue = findViewById(R.id.editFirstValue);
+        editTextSecondValue = findViewById(R.id.editSecondValue);
+
+        textViewAnswer = findViewById(R.id.textViewAnswer);
+
+        add = findViewById(R.id.buttonAdd);
+        subtract = findViewById(R.id.buttonSubtract);
+        multiply = findViewById(R.id.buttonMultiply);
+        divide = findViewById(R.id.buttonDivide);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double firstValue, secondValue, answer;
+                firstValue = Double.parseDouble(editTextFirstValue.getText().toString());
+                secondValue = Double.parseDouble(editTextSecondValue.getText().toString());
+                answer = firstValue + secondValue;
+                textViewAnswer.setText("Ans is = " + answer);
+            }
+        });
+
+        subtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double firstValue, secondValue, answer;
+                firstValue = Double.parseDouble(editTextFirstValue.getText().toString());
+                secondValue = Double.parseDouble(editTextSecondValue.getText().toString());
+                answer = firstValue - secondValue;
+                textViewAnswer.setText("Ans is = " + answer);
+            }
+        });
+
+        multiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double firstValue, secondValue, answer;
+                firstValue = Double.parseDouble(editTextFirstValue.getText().toString());
+                secondValue = Double.parseDouble(editTextSecondValue.getText().toString());
+                answer = firstValue * secondValue;
+                textViewAnswer.setText("Ans is = " + answer);
+            }
+        });
+
+        divide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double firstValue, secondValue, answer;
+                firstValue = Double.parseDouble(editTextFirstValue.getText().toString());
+                secondValue = Double.parseDouble(editTextSecondValue.getText().toString());
+                answer = firstValue / secondValue;
+                textViewAnswer.setText("Ans is = " + answer);
+            }
+        });
     }
 }
